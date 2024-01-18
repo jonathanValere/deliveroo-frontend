8231e88078e9e775d865343d963444e4c49c04fc
“Jonathan <jonathan.valere@gmail.com>
2024-01-17 22:50:23 -0300

    handler buttom add/remove quantity fixed

--- src/components/Cart/Item.jsx
+++ src/components/Cart/Item.jsx
@@ -3,28 +3,30 @@ import styles from "./Item.module.css";
 
 //Composant du repas dans le panier (Cart) ----------------
 // ⚠️ A FAIRE :
-// Lorsque la quantité change, le total doit être modifié
 // Gérer la quantité "0", le plat doit disparaître (composant Item détruit)
 
 export default function Item({ ...props }) {
   const { name, price, total, setTotal } = props;
 
-  const [quantity, setQuantity] = useState(1); // State de la quantité choisi
+  const [quantity, setQuantity] = useState(1); // State de la quantité choisie
   const priceToNumber = Number(price); // Conversion de la chaîne de cractère en nombre pour effectuer les calculs
   const [priceItem, setPriceItem] = useState(priceToNumber); // State de la valeur du repas de type Number
 
   // Hook déclenché au moment où quantity est mis à jour. Il modifie la valeur du repas
+
   useEffect(() => {
     const newPrice = priceToNumber * quantity;
     setPriceItem(newPrice);
   }, [quantity]);
 
-  // Fonction permettant de gérer la quantité ----
+  //Fonction permettant de gérer la quantité ----
   const handleClickQuantity = (operator) => {
     if (operator === "+") {
-      setQuantity(quantity + 1);
+      setQuantity((prev) => prev + 1);
+      setTotal((prev) => prev + priceToNumber);
     } else {
-      setQuantity(quantity - 1);
+      setQuantity((prev) => prev - 1);
+      setTotal((prev) => prev - priceToNumber);
     }
   };
 
